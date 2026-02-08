45176499bf6d16c2e97968bb955ece53f7f18055
Jahaan <jeyubhavsar@gmail.com>
2026-02-07 22:38:39 -0500

    added speech recognition

--- Jarvis.py
+++ Jarvis.py
@@ -1,5 +1,7 @@
 import sys
 import os
+import pyttsx3
+import speech_recognition as sr
 from openrouter import OpenRouter
 
 class JARVIS:
@@ -7,31 +9,49 @@ class JARVIS:
         # load in environment variables from system (api_key(s))
         self.api_key = os.getenv("OPENROUTER_API_KEY")
 
+        # initialize the text-to-speech module
+        self.speech = pyttsx3.init()
+
+        #initialize the speech-to-text module
+        self.recognizer = sr.Recognizer()
+
         #debug if the api_key returned null
         if not self.api_key:
             raise RuntimeError("not found")
+        
     
-    def jarvis(self):
+        
+    def speech_to_text(self):
+        with sr.Microphone() as microphone:
+            audio = self.recognizer.listen(microphone)
+
+        text = self.recognizer.recognize_google(audio)
+
+        return text
+
+    
+    def jarvis(self, text):
         client = OpenRouter(
           api_key = self.api_key,
             server_url="https://ai.hackclub.com/proxy/v1",
         )
 
-        question = " ".join(sys.argv[1:]) 
-
         response = client.chat.send(
             model="google/gemini-3-flash-preview",
             messages=[
-                {"role": "assistant", "content": question}
+                {"role": "assistant", "content": text}
             ],
         )
+        
+        answer = print(response.choices[0].message.content)
 
-        return response
+        return answer
 
     def answer(self):
-        response = self.jarvis()
-        answer = print(response.choices[0].message.content)
-        return answer
+        question = self.speech_to_text()
+
+        answer = self.jarvis(question)
+        print(f"Jarvis: {answer}")
 
 jarvis1 = JARVIS()
 jarvis1.answer()
